9ff6308b23c06e68ddc57d848376a850163cf72a
pberus <pedroberustein03@hotmail.com>
2024-01-16 15:54:18 -0300

    feat(Client/redux): init redux

---
+++ Client/src/redux/reducer.js
@@ -0,0 +1,10 @@
+const initialState = {};
+
+const rootReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    default:
+      return { ...state };
+  }
+};
+
+export default rootReducer;
