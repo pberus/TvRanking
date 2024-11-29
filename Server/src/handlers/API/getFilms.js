f0ff14bbdecfc771a589e6c50284523c06a0fa54
Pedro <pedroberustein03@hotmail.com>
2024-11-28 21:15:10 -0300

    fix(server/handler API): getFilms re-rendering

--- Server/src/handlers/API/getFilms.js
+++ Server/src/handlers/API/getFilms.js
@@ -1,29 +1,31 @@
 const getApiFilmsController = require("../../controllers/API/getFilms");
 const { Film } = require("../../db");
 
-const getApiFilmsHandler = async (req, res) => {  
+const getApiFilmsHandler = async (req, res) => {
   try {
-    const films = await getApiFilmsController();    
-    const filmsToInsert = films?.map(
-      ({
-        id,
-        original_title,
-        release_date,
-        poster_path,
-        overview,
-        vote_average
-      }) => ({
-        id,
-        title: original_title,
-        year: release_date,
-        image: poster_path,
-        overview,
-        rating: vote_average
-      })
-    );
-    console.log("Se aplica handler");
-    
-    await Film.bulkCreate(filmsToInsert);
+    const filmsExist = await Film.findOne()
+    if (!filmsExist) {
+      const films = await getApiFilmsController();    
+      const filmsToInsert = films?.map(
+        ({
+          id,
+          original_title,
+          release_date,
+          poster_path,
+          overview,
+          vote_average
+        }) => ({
+          id,
+          title: original_title,
+          year: release_date,
+          image: poster_path,
+          overview,
+          rating: vote_average
+        })
+      );
+      await Film.bulkCreate(filmsToInsert);
+    }
+
     const allFilms = await Film.findAll()
     return res.json(allFilms)
   } catch (error) {
