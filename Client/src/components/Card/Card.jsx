81b6e6ba4ef7a61e4b42df4d6c8598a3e4625ed4
Pedro <pedroberustein03@hotmail.com>
2024-12-20 18:09:32 -0300

    feat(client): hover icons in card

--- Client/src/components/Card/Card.jsx
+++ Client/src/components/Card/Card.jsx
@@ -1,6 +1,9 @@
 import PropTypes from "prop-types";
 import style from "./card.module.css";
-import noImageAvailable from "../../assets/no_image_available.jpg"
+import noImageAvailable from "../../assets/no_image_available.jpg";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import DoneIcon from "@mui/icons-material/Done";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 
 const IMAGE_URL = "https://image.tmdb.org/t/p/w500";
 
@@ -9,6 +12,17 @@ const Card = ({ tv }) => {
   return (
     <div className={style.Card}>
       <img src={image ? IMAGE_URL + image : noImageAvailable} alt={title} />
+      <div className={style.icons}>
+        <button className={style.iconsButton}>
+          <BookmarkIcon className={style.icon} />
+        </button>
+        <button className={style.iconsButton}>
+          <DoneIcon className={style.icon} />
+        </button>
+        <button className={style.iconsButton}>
+          <ThumbUpIcon className={style.icon} />
+        </button>
+      </div>
     </div>
   );
 };
